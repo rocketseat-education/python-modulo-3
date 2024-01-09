8878ae4edd6b3f2653655773757e0c3ac68b45c1
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-09 14:20:17 -0300

    Desenvolvimento do CRUD com Flask - Read

--- app.py
+++ app.py
@@ -19,5 +19,23 @@ def create_task():
   print(tasks)
   return jsonify({"message": "Nova tarefa criada com sucesso"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list = [task.to_dict() for task in tasks]
+
+  output = {
+    "tasks": task_list,
+    "total_tasks": len(task_list)
+  }
+  return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+  
+  return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
 if __name__ == "__main__":
   app.run(debug=True)
