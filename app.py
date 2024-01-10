36f943a3aa776f8144843653a40bf5378be1a2a1
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-10 14:35:27 -0300

    Desenvolvimento do CRUD com Flask - Delete

--- app.py
+++ app.py
@@ -42,6 +42,8 @@ def update_task(id):
   for t in tasks:
     if t.id == id:
       task = t
+      break
+    
   if task == None:
     return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
@@ -50,6 +52,20 @@ def update_task(id):
   task.description = data['description']
   task.completed = data['completed']
   return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+    if not task:
+      return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
   app.run(debug=True)
