78c3f4b0bfa5f48a7e67c9ca09307de7c89785df
Tamires Santos <tfs.tamsantos@gmail.com>
2024-01-11 07:34:45 -0300

    Testes automatizados com Pytest - Update e Delete

--- testes.py
+++ testes.py
@@ -31,3 +31,33 @@ def test_get_task():
         assert response.status_code == 200
         response_json = response.json()
         assert task_id == response_json['id']
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "completed": True,
+            "description": "Nova descrição",
+            "title": "Título atualizado"
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        # Nova requisição a tarefa especifica
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["title"] == payload["title"]
+        assert response_json["description"] == payload["description"]
+        assert response_json["completed"] == payload["completed"]
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        response.status_code == 200
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
